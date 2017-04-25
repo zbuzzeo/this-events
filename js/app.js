f1fc516648adeaaa1d4a6063f6fa5d16c5064e9f
victorlhlee <victorlhlee@users.noreply.github.com>
2017-04-24 20:01:15 -1000

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Secret Menu Challenge
+//Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
