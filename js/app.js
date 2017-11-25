5beedc52cad5a7ee90fa9374b95e02d0787e1c8e
victorlhlee <victorlhlee@users.noreply.github.com>
2017-11-24 17:59:38 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-//Secret Menu Challenge!
-/*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+//Secret Menu Challenge
+//Add event listeners to the following:
 
 
+/*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
+
+
+
+
+
+/*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
